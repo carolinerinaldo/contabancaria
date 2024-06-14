4bf3658c6acbb493da510cc5f309afce147be6b8
Caroline <carolinebauru@hotmail.com>
2024-06-14 16:34:31 -0300

    task 4 e 5

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,7 +1,8 @@
 package conta;
 
 import java.util.Scanner;
-import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 import conta.util.Cores;
 
 public class Menu {
@@ -9,16 +10,24 @@ public class Menu {
 	public static void main(String[] args) {
 						
 		Scanner sc = new Scanner(System.in);
-		
-		conta.model.Conta c1 = new conta.model.Conta(1, 123, 1, "Adriana", 10000.0f);
-		
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
 
 		int opcao;
+        
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+        // Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+        cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		while (true) {
 			System.out.println(
